c2456d0fbaa90173a9d3ac80d252635e7f0e52ac
Clovis KOUOI <117636695+HTClaus@users.noreply.github.com>
2024-05-30 12:29:58 +0200

    Add google translate

--- game/Translation.py
+++ game/Translation.py
@@ -1,22 +1,9 @@
+from googletrans import Translator
+
 # Transmate.py
-translations = {
-    "English": {
-        "start_button": "Start",
-        "settings_button": "Settings",
-        "instruction_button": "Instructions",
-        # Add translations for other text elements
-    },
-    "Français": {
-        "start_button": "Démarrer",
-        "settings_button": "Paramètres",
-        "instruction_button": "Instructions",
-        # Add translations for other text elements
-    },
-    "Español": {
-        "start_button": "Comenzar",
-        "settings_button": "Configuración",
-        "instruction_button": "Instrucciones",
-        # Add translations for other text elements
-    },
-    # Add translations for other languages
-}
+
+def translate_text(text, dest):
+    translator = Translator()
+    translation = translator.translate(text, dest=dest)
+    return translation.text
+
