6563b972595afe6117738a0df5caeaa846f205e0
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2024-05-31 15:13:34 +0200

    still trying to update data on api

--- game/updateData.py
+++ game/updateData.py
@@ -3,18 +3,21 @@ import requests
 def updateData(data):
     # Define the URL
     url = "http://eliascastel.ddns.net:3001/game/data"
-
+    print(type(data))
     try:
+        for elements in data:
+            data[elements] = str(data[elements])
         
+        print(data)
         # Make the API request
-        response = requests.put(url, data=data)
+        request = requests.put(url, json=data)
 
         # Process the response
-        data = response.json()
+        response = request.json()
         
-        # Print the data
-        print("Data updated successfully")
-        print(data)
-        return data
+        # Print the response
+        print("Data tried to update XD")
+        print(response)
+        return response
     except requests.exceptions.JSONDecodeError:
         print("Invalid JSON data received")
